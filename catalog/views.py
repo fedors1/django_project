10e97d163c1bc3feb0b07a863b0fab021d0db760
Konstantin Korovin <ryan337@mail.ru>
2025-06-15 12:38:52 +0300

    fixed-third-two

--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from catalog.models import Product
 
@@ -23,6 +23,6 @@ def products_list(request):
 
 
 def product_detail(request, pk):
-    product = Product.objects.get(pk=pk)
+    product = get_object_or_404(Product ,pk=pk)
     context = {"product": product}
     return render(request, "catalogs/product_detail.html", context)
